2532bdbe9afae346281ee3068cad28a38a65cf5e
mohammademon393 <mdemon357ep@gmail.com>
2025-10-08 11:41:52 +0600

    delete a component and create root

--- src/Root/Root.jsx
+++ src/Root/Root.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Navbar from '../Components/Navbar';
+import Footer from '../Components/Footer'
 import { Outlet } from 'react-router';
-import Footer from '../Components/Footer';
 
 const Root = () => {
     return (
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import { createBrowserRouter } from "react-router";
 import Home from '../Pages/Home';
-import Root from '../Pages/Root';
+import App from '../Pages/App';
+import Root from '../Root/Root';
 
 const router = createBrowserRouter([
   {
     path: "/",
     Component: Root,
+    children: [
+        {
+            path: "/",
+            Component: Home
+        },
+      {
+        path: "/app",
+        Component: App
+      },
+    ],
   },
 ]);
 
