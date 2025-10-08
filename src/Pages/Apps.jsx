069aacc2c573b71d3a39653f487f69766ecec728
mohammademon393 <mdemon357ep@gmail.com>
2025-10-08 11:55:56 +0600

    create jsx compo and rename apps jsx

--- src/Pages/Apps.jsx
+++ src/Pages/Apps.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
-const App = () => {
+const Apps = () => {
+    
     return (
         <div>
             <h1>app</h1>
@@ -8,4 +9,4 @@ const App = () => {
     );
 };
 
-export default App;
+export default Apps;
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,21 +1,28 @@
 import React from 'react';
 import { createBrowserRouter } from "react-router";
 import Home from '../Pages/Home';
-import App from '../Pages/App';
+import Apps from '../Pages/Apps';
 import Root from '../Root/Root';
+import Install from '../Pages/Install';
+import ErrorPage from '../Pages/ErrorPage';
 
 const router = createBrowserRouter([
   {
     path: "/",
     Component: Root,
+    errorElement: <ErrorPage></ErrorPage>,
     children: [
-        {
-            path: "/",
-            Component: Home
-        },
       {
-        path: "/app",
-        Component: App
+        index: true,
+        Component: Home,
+      },
+      {
+        path: "/apps",
+        Component: Apps,
+      },
+      {
+        path: "/install",
+        Component: Install,
       },
     ],
   },
